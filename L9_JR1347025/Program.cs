ae7b9cf31e37d2612c09e08388448951ed8fe9e5
Jorge Rodríguez <147176496+joge193@users.noreply.github.com>
2025-03-12 09:52:10 -0600

    Add files via upload

---
+++ L9_JR1347025/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program {
+ static void Main() {
+
+    
+ // Ejercicio 1
+ Console.WriteLine("Ingrese el monto total de la compra ");
+ int precio = int.Parse(Console.ReadLine());
+ 
+if (precio >= 100)
+{
+precio * 0.80
+ Console.WriteLine("el precio con el 20% de descuento es: " + precio);
+ }
+
+
+ }
+}
